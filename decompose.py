0f66f9fe9e3eb9dc3ddbdb5f43dbe213b3e7abd4
Matthew Theisen <mtheisen@ucla.edu>
2016-06-14 17:00:31 -0700

    special

---
+++ decompose.py
@@ -0,0 +1,22 @@
+def decompose(n):
+    if '/' in n:
+        strs = n.split('/')
+        a = int(strs[0]); b = int(strs[1]);
+    elif '.' in n:
+        strs = n.split('.')
+        b = 10**len(strs[1]);a = int(strs[1]) + b*int(strs[0]); 
+    else: a=int(n); b=1
+    output = []
+    while a >= b:
+        i = a//b; a = a - b*i;
+        output.append(str(i))
+    while a > 0:
+        i = b//a+1 if b%a else b//a;
+        a = a*i-b; b = b*i
+        output.append('1/'+str(i))
+    return output
+
+print decompose('2/3')
+print decompose('0.66')
+print decompose('75/3')
+print decompose('1.25')
